17e7185f6ff3ffbc4fe16be559fc2932eef3c70d
ManuelArdid <manuelardid0@gmail.com>
2026-01-20 20:28:54 +0100

    Added doubkle jump animation

--- Assets/Scripts/Character/Movement.cs
+++ Assets/Scripts/Character/Movement.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Movement : MonoBehaviour
 {
     //------- Unity Editor Variables -------//
@@ -27,14 +29,21 @@ public class Movement : MonoBehaviour
 
     //------- Private Variables -------//
     private Rigidbody2D _rb;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
     private Vector2 _rawMovementInput;
     private bool _jumpRequested = false;
     private Vector2 _currentVelocity = Vector2.zero;
+    private bool _onPlatform = false;
+    private bool _isJumping = false;
+    private bool _isdoubleJumping = false;
 
     //------- Unity Methods -------//
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
@@ -57,12 +66,46 @@ public class Movement : MonoBehaviour
         //JUMP
         if (_jumpRequested)
         {
-            if (IsGrounded())
+            _rb.linearVelocityY = 0f;
+            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            _isJumping = true;
+
+
+            //Double jump
+            if (!IsGrounded())
             {
-                _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                //animation trigger
+                _animator.SetTrigger("PerformDoubleJump");
+                _isdoubleJumping = true;
             }
+
             _jumpRequested = false;
         }
+
+        //Ground check reset
+        if (IsGrounded())
+        {
+            _isJumping = false;
+            _isdoubleJumping = false;
+        }
+
+        //ANIMATIONS
+
+        //Running
+        _animator.SetBool("IsRunning", _currentVelocity.x != 0 && IsGrounded());
+
+        //Falling
+        _animator.SetBool("IsFalling", !IsGrounded() && _rb.linearVelocityY < 0f && !_isJumping && !_isdoubleJumping);
+
+        //Jumping
+        _animator.SetBool("IsJumping", !IsGrounded() && _rb.linearVelocityY > 0f && _isJumping   && !_isdoubleJumping);
+
+        //Flip sprite
+        if (_currentVelocity.x > 0)
+            _spriteRenderer.flipX = false;
+        else if (_currentVelocity.x < 0)
+            _spriteRenderer.flipX = true;
+
     }
 
     void OnEnable()
@@ -75,7 +118,6 @@ public class Movement : MonoBehaviour
         MovementInputAction.action.canceled += Move;
         MovementInputAction.action.started += Move;
 
-        JumpInputAction.action.performed += Jump;
         JumpInputAction.action.started += Jump;
         JumpInputAction.action.canceled += JumpCancelled;
     }
@@ -90,10 +132,26 @@ public class Movement : MonoBehaviour
         MovementInputAction.action.canceled -= Move;
         MovementInputAction.action.started -= Move;
 
-        JumpInputAction.action.performed -= Jump;
         JumpInputAction.action.started -= Jump;
         JumpInputAction.action.canceled -= JumpCancelled;
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            _onPlatform = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            _onPlatform = false;
+        }
+    }
+
 
     //------- Private Methods -------//
 
@@ -113,6 +171,9 @@ public class Movement : MonoBehaviour
         _jumpRequested = true;
     }
 
+    /// <summary>
+    /// Handles jump cut for variable jump height.
+    /// </summary>
     void JumpCancelled(InputAction.CallbackContext context)
     {
         if (_rb.linearVelocityY > 0f)
@@ -120,11 +181,16 @@ public class Movement : MonoBehaviour
             var currentVelocityY = _rb.linearVelocityY;
             _rb.linearVelocityY = currentVelocityY * jumpCutMultiplier;
         }
+
+        _isJumping = false;
     }
 
+    /// <summary>
+    /// Checks if the character is grounded.
+    /// </summary>
     bool IsGrounded()
     {
         RaycastHit2D hit = Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckDistance, groundLayer);
-        return hit.collider != null;
+        return hit.collider != null || _onPlatform;
     }
 }
